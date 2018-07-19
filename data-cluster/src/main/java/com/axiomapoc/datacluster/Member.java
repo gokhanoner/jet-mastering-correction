15e3d2714be73ef738fd971bafe5e868867b1c3b
Gökhan Öner <gokhanoner@gmail.com>
2018-07-18 17:09:05 -0700

    Update Member.java

--- data-cluster/src/main/java/com/axiomapoc/datacluster/Member.java
+++ data-cluster/src/main/java/com/axiomapoc/datacluster/Member.java
@@ -18,7 +18,6 @@ public class Member {
 
     public static void main(String[] args) {
         System.setProperty("hazelcast.phone.home.enabled", "false");
-        System.setProperty("hazelcast.enterprise.license.key","HazelcastEnterpriseHD#2Nodes#maOHFiwR5YEcy1T6K7bJ0u290q21h9d19g00sX99C39399eG99Z9v0x9t9x0");
 
         Config config = new Config();
         config.getGroupConfig().setName(DATA_CLUSTER_NAME);
